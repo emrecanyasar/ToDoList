771bfbd122c7566ddb8af3310119a6ef313eb1d2
Emre Can Yasar <emrecanyasar8@outlook.com>
2023-05-04 16:41:33 +0300

    some fixes

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let addCounter = 2;
+
 function onDragStart(event) {
   event.dataTransfer.setData("text", event.target.id);
 
@@ -23,33 +25,114 @@ function onDrop(event) {
   }
 }
 
+function onTodo(event) {
+  const id = event;
+  const draggableElement = document.getElementById(id);
+  const grandElement = draggableElement.parentElement.parentElement;
+  console.log(grandElement);
+
+  const dropzone1 = document.getElementById("tddropzone");
+
+  dropzone1.appendChild(grandElement);
+
+//   console.log(draggableElement.parentElement.parentElement.parentElement);
+//   if (
+//     draggableElement.parentElement.parentElement.parentElement.className ===
+//     "dropzone"
+//   ) {
+//     const doingRemove = document.getElementById(event);
+//     doingRemove.style.display = "none";
+//     const todoAdd = document.getElementById("btn-doing");
+//     todoAdd.style.display = "flex";
+//     todoAdd.style.flexDirection = "row";
+//     const doneAdd = document.getElementById("btn-done");
+//     doneAdd.style.display = "flex";
+//   }
+}
+
 function onDoing(event) {
-  // const id = this.parentElement.getAttribute("id");
-  console.log("event", event.target);
-  console.log("asdad", this.document.body.parentElement);
-
-
-  const draggableElement = document.getElementById("draggable-1");
-
-  console.log(draggableElement);
-  let dropzone = "";
+  const id = event;
+  const draggableElement = document.getElementById(id);
+  const grandElement = draggableElement.parentElement.parentElement;
+  console.log(grandElement);
 
   const dropzone1 = document.getElementById("doingdropzone");
 
-  console.log(dropzone1);
-  dropzone1.appendChild(draggableElement);
+  dropzone1.appendChild(grandElement);
+
+  console.log(draggableElement.parentElement.parentElement.parentElement);
+  if (
+    draggableElement.parentElement.parentElement.parentElement.className ===
+    "dropzone1"
+  ) {
+    const doingRemove = document.getElementById(event);
+    doingRemove.style.display = "none";
+    // const btnIdTodo = "btn-todo-" + (addCounter-1)
+    // const todoAdd = document.getElementById(btnIdTodo);
+    // todoAdd.style.display = "flex";
+    const btnIdDone = "btn-done-" + (addCounter-1)
+    const doneAdd = document.getElementById(btnId);
+    doneAdd.style.display = "flex";
+  }
 }
 
+// const deneme = document.getElementsByClassName("tdmaincardbox")
+
+// if(deneme.parentElement==="dropzone1"){
+//      const btnIdTodo = "btn-todo-" + (addCounter-1)
+//     const todoAdd = document.getElementById(btnIdTodo);
+//     todoAdd.style.display = "flex";
+// }
+
+
+function onDone(event) {
+  const id = event;
+  const draggableElement = document.getElementById(id);
+  const grandElement = draggableElement.parentElement.parentElement;
+  console.log(grandElement);
+
+  const dropzone1 = document.getElementById("donedropzone");
+
+  dropzone1.appendChild(grandElement);
+
+//   console.log(
+//     draggableElement.parentElement.parentElement.parentElement.className
+//   );
+//   if (
+//     draggableElement.parentElement.parentElement.parentElement.className ===
+//     "dropzone2"
+//   ) {
+//     const doingRemove = document.getElementById(event);
+//     doingRemove.style.display = "none";
+//     const todoAdd = document.getElementById("btn-todo");
+//     todoAdd.style.display = "flex";
+//     todoAdd.style.flexDirection = "row";
+//     const doingAdd = document.getElementById("btn-doing");
+//     doingAdd.style.display = "flex";
+//     doingAdd.style.flexDirection = "row";
+//   }
+}
 
 function addCard() {
-    if (onclick=true) {
-        console.log("asdas");
-    }
-    const originalDiv = document.querySelector('#draggable-1');
-    const div = document.createElement('div');
-    const copiedDiv = originalDiv.cloneNode(true);
-    // div.innerHTML ="asdasd"
-    
-    const dropzone = document.getElementById("tddropzone");
-    dropzone.appendChild(copiedDiv);
+  if ((onclick = true)) {
+  }
+  const originalDiv = document.querySelector("#draggable-1");
+  const div = document.createElement("div");
+  const copiedDiv = originalDiv.cloneNode(true);
+  console.log(copiedDiv.lastChild);
+  copiedDiv.id = "draggable-" + addCounter;
+  console.log(copiedDiv.id);
+  const btnDoing = copiedDiv.querySelector("#btn-doing");
+  btnDoing.id = "btn-doing-" + addCounter;
+  console.log(btnDoing.id);
+  const btnTodo = copiedDiv.querySelector("#btn-todo");
+  btnTodo.id = "btn-todo-" + addCounter;
+  btnTodo.style.display ="none"
+  console.log(btnTodo.id);
+  const btnDone = copiedDiv.querySelector("#btn-done");
+  btnDone.id = "btn-done-" + addCounter;
+  console.log(btnDone.id);
+  const dropzone = document.getElementById("tddropzone");
+  dropzone.appendChild(copiedDiv);
+  addCounter = addCounter + 1;
 }
